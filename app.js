96647433ed9146b4771f844aa8b723045fa8ca59
UtkarshPrakash <utkarshprakash21sep@gmail.com>
2024-12-19 23:28:28 +0530

    added environment port selection

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
 
-PORT = 3000;
+PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("Listening on localhost port 3000");
