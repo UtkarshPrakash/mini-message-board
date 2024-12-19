79d27553c5aab324e006a5d5cf3fff4b4c8d6266
UtkarshPrakash <utkarshprakash21sep@gmail.com>
2024-12-19 23:16:56 +0530

    add all files

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("node:path");
+const router = require("./routes/index");
+
+app = express();
+const assetsPath = path.join(__dirname, "public");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+
+PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("Listening on localhost port 3000");
+})
+
+
